18cc8b72ad8c68372b8b7f9ce402f4286f23df0d
Nguyen Trung <nguyentrung0904@gmail.com>
2016-05-26 01:27:22 +0800

    1. Call MS API to get Image ID from URL 2. Compare the received image id to the prepared ones to make decision

--- CallAPI.py
+++ CallAPI.py
@@ -1,27 +1,46 @@
 import httplib
+import json
 import urllib
 
 
 class FaceDetection:
-    def __init__(self, apiUrl = "https://api.projectoxford.ai/face/v1.0", key = "d2495e1c650941ecbf2598373f5243c4"):
+    def __init__(self, apiUrl="api.projectoxford.ai", key="d2495e1c650941ecbf2598373f5243c4"):
         self.apiKey = key
         self.apiUrl = apiUrl
-        self.imagesPatternId = ["faceid1", "faceid2", "faceid3"]
+        self.imagesPatternId = ["c594c66a-4442-4860-963a-ebc9a5d82ead"]
 
     def getFaceId(self, imageUrl):
         param = {
             "returnFaceId": True,
         }
         body = {"url": imageUrl}
-        method = "/detect"
+        method = "/face/v1.0/detect"
         returnData = self.sendHTTPSRequest(param, method, body)
-        print returnData
+        dict = returnData[0]
+        faceId = dict['faceId']
+        print faceId
+        return faceId
 
-    def compare2Faces(self, imagesId):
-        param = "{}"
-        body = "{}"
-        method = "/verify"
+    def compare2Faces(self, imageId1, imageId2):
+        param = {}
+        body = {
+            "faceId1": imageId1,
+            "faceId2": imageId2
+        }
+        method = "/face/v1.0/verify"
         returnData = self.sendHTTPSRequest(param, method, body)
+        isIdentical = returnData["isIdentical"]
+        result = 0
+        if (isIdentical):
+            result = returnData["confidence"]
+        print result
+        return result
+
+    def identifyFace(self, imageUrl):
+        faceId = self.getFaceId(imageUrl)
+        for imageId in self.imagesPatternId:
+            result = self.compare2Faces(faceId, imageId)
+            print result
 
     def sendHTTPSRequest(self, param, method, body):
         headers = {
@@ -29,27 +48,28 @@ class FaceDetection:
             'Content-Type': 'application/json',
             'Ocp-Apim-Subscription-Key': self.apiKey
         }
-
         params = urllib.urlencode(param)
-
         try:
             conn = httplib.HTTPSConnection(self.apiUrl)
-            conn.request("POST", "%s?%s" % (method, params), body, headers)
+            print "Log: Start sending http request to URL: "
+            conn.request("POST", "%s?%s" % (method, params), json.dumps(body), headers)
             response = conn.getresponse()
+            print "Log: Got result from URL"
             data = response.read()
-            print(data)
+            dict = json.loads(data)
+            print "Log: Result %s" % dict
             conn.close()
-            return data
+            return dict
         except Exception as e:
             print("[Error {0}] {1}".format(e.message, e.message))
         return ""
 
+
 def main():
     fd = FaceDetection()
     imageUrl = "https://www2.rsna.org/timssnet/media/pressreleases/images/Rybicki2-lg.jpg"
-    fd.getFaceId(imageUrl)
+    fd.identifyFace(imageUrl)
+
 
 if __name__ == "__main__":
     main()
-
-
